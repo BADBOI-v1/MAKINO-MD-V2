f8d527041f08af34a6157d27ba5996d316f5ab56
Ikechukwu <165565859+BADBOI-v1@users.noreply.github.com>
2024-07-07 15:08:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const chalk = require("chalk");
 //to disable function - false
 //
 global.available = false;
-global.autoReadGc = false;
+global.autoReadGc = true;
 global.autoReadAll = false;
 global.antitags = false;
 
@@ -14,14 +14,14 @@ global.antitags = false;
 global.autoTyping = false;                //make true to enable auto typing
 global.autoRecord = true;                //make true to enable auto recording
 global.groupevent = true;                //This is the new variable for controlling group event handling.
-global.statusseen = false;                 //make true to view statuses 
+global.statusseen = true;                 //make true to view statuses 
 
 
 global.Owner = ["2348140825959"];
 global.OwnerNumber = ["2348140825959"];
 global.ownertag = [""]; 
-global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
-global.BotName = "🐦Makino-md-v2";
+global.OwnerName =  "BAD BOI";
+global.BotName = "🐦BADBOI-md-v2";
 global.packname = "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";                             //Do not change.
 global.author = "🐦Makino md ᴍᴜʟᴛɪ-ᴅᴇᴠɪᴄᴇ";                               //Do not change.
 global.BotSourceCode = "https://github.com/anonphoenix007/MAKINO-MD-V2"; //Do not change.
@@ -30,7 +30,7 @@ global.SupportGroupLink = "https://chat.whatsapp.com/KEg0G3UUs1G39ikdyfF5Pm"; //
 //global.dbase = process.env.DBASE || "";//Mongodb here
 global.AuthorWhatsapp = "https://wa.me/2347080968564"; //Bot creator WhatsApp link,don't change or bot will not work.
 
-global.sessID = process.env.sessID || `{"noiseKey":{"private":{"type":"Buffer","data":"AJJak+LsdVH0tbULjLJdx8jnfWEHgk3UB7CXSWF0SXo="},"public":{"type":"Buffer","data":"+fYTp0yhon88Zp8D/HNETlQhHPvX8OLTsr6rWw+UJxA="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"KBk8av7Svlxc7EseCnP04s8KHXOemXKKzgcQhIXiMHA="},"public":{"type":"Buffer","data":"6sIbODNXHzgIXuhDxKxBFxUktAqMgiC5G23jSbwvqAo="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"WH/f/rTJJu7SM7fCeDU4y5kDUlmyHSI2LCdSjjF2km0="},"public":{"type":"Buffer","data":"4x8eHT0VB5J+GwoBkYeaAtgUuCKHxhLU1gOGqPiYXkw="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"SPQt4/N6ppvN4PpmPcY7BGQ2lmdbdplWAghQLefpc0c="},"public":{"type":"Buffer","data":"AwXSO+pjaY1trIrbVhXhPfXU17WmfKhAU0Oe8DQCrBk="}},"signature":{"type":"Buffer","data":"vwJWzP/PyHqpuHyUfTVNoNvyVbiFbCDJcNJgyXtjyPq4c+l+Ctoy80gdJRmLExh9m3xbw7cizzZnjxF968a+gw=="},"keyId":1},"registrationId":219,"advSecretKey":"cRzlZDnLiQBJRjPsJCdrPYm1sUf8+VrEoAknWHmoAuo=","processedHistoryMessages":[],"nextPreKeyId":31,"firstUnuploadedPreKeyId":31,"accountSyncCounter":0,"accountSettings":{"unarchiveChats":false},"deviceId":"LrfPtrq7RcuT6aqIWzv0xQ","phoneId":"b42ea214-52ae-4ba0-a2d2-8a17aa61a5ad","identityId":{"type":"Buffer","data":"wshh18Y5BGrosepRoYDuJNNMuV8="},"registered":true,"backupToken":{"type":"Buffer","data":"71QHThXdmnx5yXogkyx8n4DsdXo="},"registration":{},"pairingCode":"6BTWVPLV","me":{"id":"2347063183606:12@s.whatsapp.net","name":"Ikechukwu"},"account":{"details":"CMrBrpgFENfEpLQGGAEgACgA","accountSignatureKey":"LXexUPxGoQWh691wsKzwxIp1/9jmsKFYoc0VQsBMdwg=","accountSignature":"0mHJLLRR5KNj+xkoSlv07nep9vs5Y4M4E39BDiyJlRvjpW9sUyBhrBij6aFa+SJ7GTAI+G5Bb+72DGqTA/n5AA==","deviceSignature":"3x4+mi02yYfijkb1Q9Rtmxrx9U7Kh+et+W6iBIvUNXrDeN1P6paVtZ4I/o299gCTdNOncRo9F6RW4KA299lsiw=="},"signalIdentities":[{"identifier":{"name":"2347063183606:12@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"BS13sVD8RqEFoevdcLCs8MSKdf/Y5rChWKHNFULATHcI"}}],"platform":"smba","lastAccountSyncTimestamp":1720263269,"myAppStateKeyId":"AAAAABkp"}`
+global.sessID = process.env.sessID || `{"noiseKey":{"private":{"type":"Buffer","data":"2Fur/9ea1cfC8WVYLqO9XrfcoUk+L6e4BUMRLu/q1Fk="},"public":{"type":"Buffer","data":"1lIJwOPGJiLLIBiECnsFL0jZPkbFZOyl0P/XkG/fRRo="}},"pairingEphemeralKeyPair":{"private":{"type":"Buffer","data":"aD3swLregB0dIxKhOD+gHMYDXF400MSP8OHr9yFTNEU="},"public":{"type":"Buffer","data":"eSStseLgUgK7u/uJNsNj3M2JFuQ3un1A/DoMy/vDyno="}},"signedIdentityKey":{"private":{"type":"Buffer","data":"cOd/hgAoGtfEDkWCUkig5fAjx8slO8sF5K3yXR4iQEw="},"public":{"type":"Buffer","data":"wUBugRJE4AYocHS3NM0a33hb1v3try9VnolF4p0bFEU="}},"signedPreKey":{"keyPair":{"private":{"type":"Buffer","data":"yL/G9+m1NhucAGBl1+7QqFy+sIhJ/YoqcyHaGBZkt3s="},"public":{"type":"Buffer","data":"dR8VoLJGYu7i2ElDC1+8HMNnatBNdnBKqwGpCjdjgXU="}},"signature":{"type":"Buffer","data":"tICnvz9FSO7yshIWjo002472RujCwX1/hUq/UrH89JC7khJROHdgp3kPuXk+aIFLOAezYcMZ8i2zQUSG8X0ogQ=="},"keyId":1},"registrationId":245,"advSecretKey":"HtKJAizdPBpjCEVoWwP3EHOUEuLHkhW6z8IquHqXS9Q=","processedHistoryMessages":[],"nextPreKeyId":31,"firstUnuploadedPreKeyId":31,"accountSyncCounter":0,"accountSettings":{"unarchiveChats":false},"deviceId":"X0pRWFnWT-Oens-a2tMcag","phoneId":"036bbfab-e266-4f7d-9c8b-fe5b85481d2a","identityId":{"type":"Buffer","data":"iKvJAoI5SnFcxUSXkh60jvaCeDw="},"registered":true,"backupToken":{"type":"Buffer","data":"kl2e/x5KdR/lCHOESEiKK5lx01g="},"registration":{},"pairingCode":"BPETN73C","me":{"id":"2348140825959:89@s.whatsapp.net","name":"Bad Boi Hacker"},"account":{"details":"CP7e9rADEK/AqrQGGAMgACgA","accountSignatureKey":"BVbuHshB/4TVvwO3Xg8m5XgA5FXG4dSJnqZ/LlCnMCo=","accountSignature":"AF7G7+DynLRvnbM3yiZwXkaedvxkMtREyOiBTu/Cg9DIC0QuVkzo3kwJ3j4wA38qhS/XdxvUA79qUxMK2inOhw==","deviceSignature":"3OgfNKNLSXXHliNO94djK4tEKaim3EOE4w1i5ylKYd1Hw71q6oerP3tOlnIhVC+JwhZJ8IdaLSKb7RWtpavWhg=="},"signalIdentities":[{"identifier":{"name":"2348140825959:89@s.whatsapp.net","deviceId":0},"identifierKey":{"type":"Buffer","data":"BQVW7h7IQf+E1b8Dt14PJuV4AORVxuHUiZ6mfy5QpzAq"}}],"platform":"smbi","lastAccountSyncTimestamp":1720361022,"myAppStateKeyId":"AAAAAKSL"}`
 global.prefa = ["/"]
 
 //
@@ -65,7 +65,7 @@ global.ntilink = []
 global.mess = {
     jobdone: 'Here you go...',
     useradmin: 'Only group Admin can use the command 😂 ',
-    botadmin: 'Make me Admin first 😌📍.',
+    botadmin: 'Make me BADBOI Admin first 😌📍.',
     botowner: 'Only my *Owner* can use this command,Dont trespass, Baka!',
     grouponly: 'This command is only made for *Groups*, Baka!',
     privateonly: 'This command is only made for *Private Chat*, Baka!',
